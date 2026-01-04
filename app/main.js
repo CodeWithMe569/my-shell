3067290bda27392c5469900c0f6e03397965e785
CodeWithMe569 <ht5620085@gmail.com>
2026-01-04 06:09:26 +0530

    fix syntax issue

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 const readline = require('readline');
 
-const rl = readline.createInterface(
+const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
-);
+});
 
 rl.setPrompt("$ ");
 rl.prompt();
